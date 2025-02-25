a488986f0422baab8d886144c90c15ab3e96a9cf
davids816 <davids816@nycstudents.net>
2025-02-25 16:28:32 +0000

    changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import { todoRouter } from './todos.js';
 import express from 'express';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
@@ -16,16 +17,15 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Use routes
+  // Use routesrs
 app.use('/api', todoRouter);
 
 // Basic route
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'client/index.html'));
+    res.json('Welcome to my app!');
 });
 
 // Start server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-import { todoRouter } from './todos.js';
